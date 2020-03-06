45db8ef598f5b4270d9eecebcfa7a7f157c373a6
Jeff Muizelaar <jrmuizel@gmail.com>
2020-03-06 17:57:46 -0500

    Make better

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::time::Instant;
+use std::{sync, thread};
 
 fn main() {
     let (s, r) = std::sync::mpsc::channel();
-    //s.send(Instant::now());
-    //r.recv().unwrap();
     let j = std::thread::spawn(move || {
         std::thread::sleep(std::time::Duration::from_millis(10));
         s.send(Instant::now()).unwrap();
     } );
-    let start = r.recv().unwrap();
 
+    let start = r.recv().unwrap();
     let time = Instant::now() - start;
+
     let start = Instant::now();
-    let measure = Instant::now() - start;
-    println!("Thread switch timing {} {}", time.as_secs_f32() * 1000., measure.as_secs_f32() * 1000.);
+    let precision = Instant::now() - start;
+    println!("Thread switch timing {}us {}us", time.as_secs_f32() * 1000.*1000., precision.as_secs_f32() * 1000.*1000.);
+    
+    j.join().unwrap();
 }
